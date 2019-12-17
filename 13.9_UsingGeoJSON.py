284b31e5dc0b95785a4fa44f00449eb957be1d94
AbigailJohnson2017 <54185029+AbigailJohnson2017@users.noreply.github.com>
2019-12-17 01:24:43 -0600

    Add files via upload

---
+++ 13.9_UsingGeoJSON.py
@@ -0,0 +1,53 @@
+import urllib.request, urllib.parse, urllib.error
+import json
+import ssl
+
+api_key = False
+# If you have a Google Places API key, enter it here
+# api_key = 'AIzaSy___IDByT70'
+# https://developers.google.com/maps/documentation/geocoding/intro
+
+#Working around the Google API key and using the correct API endpoint for the assignment
+if api_key is False:
+    api_key = 42
+    serviceurl = 'http://py4e-data.dr-chuck.net/json?'
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+#Input the address
+while True:
+    address = input('Enter location: ')
+    if len(address) < 1: break
+
+    parms = dict()
+    parms['address'] = address
+    if api_key is not False: parms['key'] = api_key
+    url = serviceurl + urllib.parse.urlencode(parms)
+
+    print('Retrieving', url)
+    uh = urllib.request.urlopen(url, context=ctx)
+    data = uh.read().decode()
+    print('Retrieved', len(data), 'characters')
+
+    try:
+        js = json.loads(data)
+    except:
+        js = None
+
+    if not js or 'status' not in js or js['status'] != 'OK':
+        print('==== Failure To Retrieve ====')
+        print(data)
+        continue
+
+info=json.loads(data)
+lod=info["results"]
+
+for dic in lod:
+    try:
+        ID=dic.get('place_id')
+        print(ID)
+    except:
+        continue
